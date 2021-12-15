2ec6c6f2924c0a609617d12053156c19b9e518b4
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2021-12-15 20:15:07 +0100

    Added --apply-patches and --backup-dir to the scanner

---
+++ patching/patching.go
@@ -0,0 +1,192 @@
+package patching
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/hillu/local-log4j-vuln-scanner/filter"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func log(a ...interface{}) {
+	_, err := fmt.Fprintln(os.Stderr, a...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func patch(source, dest string, examineV1 bool) error {
+	zr, err := zip.OpenReader(source)
+	if err != nil {
+		log("open (read)", source, err)
+		os.Exit(1)
+	}
+	defer func(zr *zip.ReadCloser) {
+		_ = zr.Close()
+	}(zr)
+
+	of, err := os.Create(dest)
+	if err != nil {
+		log("open (write): ", dest, err)
+		return err
+	}
+
+	zw := zip.NewWriter(of)
+	for _, member := range zr.File {
+		r, err := member.Open()
+		if err != nil {
+			log("open (read): ", source, member.Name, err)
+			if err = discardZip(dest, of, zw); err != nil {
+				return err
+			}
+		}
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, r); err != nil { //nolint:gosec
+			log("read: ", source, member.Name, err)
+			_ = discardZip(dest, of, zw)
+			return err
+		}
+		content := buf.Bytes()
+
+		if desc := filter.IsVulnerableClass(content, member.Name, examineV1); desc != "" {
+			fmt.Printf("Filtering out %s (%s)\n", member.Name, desc)
+			_ = r.Close()
+		} else {
+			w, err := zw.Create(member.Name)
+			if err != nil {
+				log("open (write): ", dest, member.Name, err)
+				_ = discardZip(dest, of, zw)
+				return err
+			}
+
+			if _, err := io.Copy(w, buf); err != nil {
+				log("write: ", dest, member.Name, err)
+				_ = discardZip(dest, of, zw)
+				return err
+			}
+			_ = r.Close()
+		}
+	}
+
+	if err := zw.Close(); err != nil {
+		log("finalize:", dest, err)
+	}
+
+	return of.Close()
+}
+
+func backupFile(src string, dest string) error {
+	fileInfo, err := os.Stat(src)
+	if err != nil {
+		return errors.New("failed to stat source file: " + err.Error())
+	}
+
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+	hdr := &tar.Header{
+		Name: src,
+		Mode: int64(fileInfo.Mode()),
+		Size: fileInfo.Size(),
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
+		return errors.New("failed to write source file header" + err.Error())
+	}
+	srcBytes, err := ioutil.ReadFile(src)
+	if err != nil {
+		return errors.New("failed to read source file" + err.Error())
+	}
+	if _, err := tw.Write(srcBytes); err != nil {
+		return errors.New("failed to write source to tar archive" + err.Error())
+	}
+	if err := tw.Close(); err != nil {
+		return errors.New("failed to close tar archive" + err.Error())
+	}
+
+	return ioutil.WriteFile(dest, buf.Bytes(), 0600)
+}
+
+func PatchAndSwap(source, backupDir string) (err error) {
+	success := false
+
+	backupFileName := strings.ReplaceAll(strings.TrimLeft(source, "/"), "/", "_") +
+		"-" + strconv.FormatInt(time.Now().Unix(), 10) + ".tar"
+	backup := filepath.Join(backupDir, backupFileName)
+	dest := source + ".new"
+
+	defer func() {
+		if !success {
+			log("Removing backup file at ", backup)
+			_ = os.Remove(backup)
+		}
+	}()
+
+	sourceFileInfo, err := os.Stat(source)
+	if err != nil {
+		return errors.New("failed stat-ing source file: " + err.Error())
+	}
+	sourceFileStat, ok := sourceFileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return errors.New("failed getting source file uid/gid: " + err.Error())
+	}
+
+	// Backup the source file
+	log("Backing up ", source, " into ", backupDir, " as ", backup)
+
+	if err := os.MkdirAll(backupDir, 0755); err != nil {
+		return errors.New("failed creating backup dir: " + err.Error())
+	}
+	if err := backupFile(source, backup); err != nil {
+		return errors.New("failed source backup: " + err.Error())
+	}
+
+	// Patch source into the temp file
+	log("Patching ", source, " into ", dest)
+	err = patch(source, dest, true)
+	if err != nil {
+		_ = os.Remove(backup)
+		return errors.New("failed patching source: " + err.Error())
+	}
+
+	if err := os.Chown(dest, int(sourceFileStat.Uid), int(sourceFileStat.Gid)); err != nil {
+		return errors.New("failed setting file owner on target: " + err.Error())
+	}
+
+	if err := os.Chmod(dest, sourceFileInfo.Mode()); err != nil {
+		return errors.New("failed setting file mode on target: " + err.Error())
+	}
+
+	// Replace source with patched temp file
+	log("Replacing source ", source, " with patched version from ", dest)
+	err = os.Rename(dest, source)
+	if err != nil {
+		_ = os.Remove(backup)
+		return errors.New("failed to stage patched file : " + err.Error())
+	}
+
+	success = true
+
+	log("Patching done for " + source)
+
+	return nil
+}
+
+func discardZip(dest string, of *os.File, zw *zip.Writer) error {
+	log("Removing output file", dest)
+	if err := zw.Close(); err != nil {
+		return err
+	}
+	if err := of.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- scanner/main.go
+++ scanner/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"github.com/hillu/local-log4j-vuln-scanner/patching"
 	"io"
 	"io/ioutil"
 	"os"
@@ -14,16 +15,20 @@ import (
 	"github.com/hillu/local-log4j-vuln-scanner/filter"
 )
 
-var logFile = os.Stdout
-var errFile = os.Stderr
+var (
+	logFile = os.Stdout
+	errFile = os.Stderr
+)
 
 func handleJar(path string, ra io.ReaderAt, sz int64) {
+	var vulnerable bool
+	vulnerable = false
 	if verbose {
-		fmt.Fprintf(logFile, "Inspecting %s...\n", path)
+		_, _ = fmt.Fprintf(errFile, "Inspecting %s...\n", path)
 	}
 	zr, err := zip.NewReader(ra, sz)
 	if err != nil {
-		fmt.Fprintf(logFile, "cant't open JAR file: %s (size %d): %v\n", path, sz, err)
+		_, _ = fmt.Fprintf(errFile, "can't open JAR file: %s (size %d): %v\n", path, sz, err)
 		return
 	}
 	for _, file := range zr.File {
@@ -31,34 +36,43 @@ func handleJar(path string, ra io.ReaderAt, sz int64) {
 		case ".class":
 			fr, err := file.Open()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(errFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
 				continue
 			}
 			buf := bytes.NewBuffer(nil)
-			if _, err = io.Copy(buf, fr); err != nil {
-				fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
-				fr.Close()
+			if _, err = io.Copy(buf, fr); err != nil { //nolint:gosec
+				_, _ = fmt.Fprintf(errFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
+				_ = fr.Close()
 				continue
 			}
-			fr.Close()
+			_ = fr.Close()
 			if desc := filter.IsVulnerableClass(buf.Bytes(), file.Name, !ignoreV1); desc != "" {
-				fmt.Fprintf(logFile, "indicator for vulnerable component found in %s (%s): %s\n", path, file.Name, desc)
+				_, _ = fmt.Fprintf(logFile, "indicator for vulnerable component found in %s (%s): %s\n", path, file.Name, desc)
+				vulnerable = true
 				continue
 			}
 
 		case ".jar", ".war", ".ear":
 			fr, err := file.Open()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(errFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
 				continue
 			}
 			buf, err := ioutil.ReadAll(fr)
-			fr.Close()
+			_ = fr.Close()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(errFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
 			}
 			handleJar(path+"::"+file.Name, bytes.NewReader(buf), int64(len(buf)))
 		}
+	}
+	if vulnerable && applyPatches {
+		err := patching.PatchAndSwap(path, backupDir)
+		if err != nil {
+			fmt.Println("Failed to patch Jar at " + path + " : " + err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Patched Jar : " + path)
 	}
 }
 
@@ -82,44 +96,57 @@ func (flags excludeFlags) Has(path string) bool {
 	return false
 }
 
-var excludes excludeFlags
-var verbose bool
-var logFileName string
-var quiet bool
-var ignoreV1 bool
+var (
+	excludes     excludeFlags
+	verbose      bool
+	logFileName  string
+	backupDir    string
+	quiet        bool
+	ignoreV1     bool
+	applyPatches bool
+)
 
 func main() {
 	flag.Var(&excludes, "exclude", "paths to exclude")
 	flag.BoolVar(&verbose, "verbose", false, "log every archive file considered")
 	flag.StringVar(&logFileName, "log", "", "log file to write output to")
-	flag.BoolVar(&quiet, "quiet", false, "no ouput unless vulnerable")
+	flag.BoolVar(&quiet, "quiet", false, "no output unless vulnerable")
 	flag.BoolVar(&ignoreV1, "ignore-v1", false, "ignore log4j 1.x versions")
+	flag.BoolVar(&applyPatches, "apply-patches", false, "automatically patch vulnerability")
+	flag.StringVar(&backupDir, "backup-dir", "", "directory to write backup files to")
 	flag.Parse()
 
 	if !quiet {
-		fmt.Printf("%s - a simple local log4j vulnerability scanner\n\n", filepath.Base(os.Args[0]))
+		_, _ = fmt.Fprintf(os.Stderr, "%s - a simple local log4j vulnerability scanner\n\n", filepath.Base(os.Args[0]))
+	}
+
+	if applyPatches && backupDir == "" {
+		_, _ = fmt.Fprintf(os.Stderr, "--apply-patches needs --backup-dir to be set\n")
+		os.Exit(1)
 	}
 
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [--verbose] [--quiet] [--ignore-v1] [--exclude path] [ paths ... ]\n", os.Args[0])
+		_, _ = fmt.Fprintf(os.Stderr, "Usage: %s [--verbose] [--quiet] [--ignore-v1] [--apply-patches] [--backup-dir path] [--exclude path] [ paths ... ]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	if logFileName != "" {
 		f, err := os.Create(logFileName)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Could not create log file")
+			_, _ = fmt.Fprintln(os.Stderr, "Could not create log file")
 			os.Exit(2)
 		}
 		logFile = f
 		errFile = f
-		defer f.Close()
+		defer func(f *os.File) {
+			_ = f.Close()
+		}(f)
 	}
 
 	for _, root := range flag.Args() {
-		filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		_ = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				fmt.Fprintf(errFile, "%s: %s\n", path, err)
+				_, _ = fmt.Fprintf(errFile, "%s: %s\n", path, err)
 				return nil
 			}
 			if excludes.Has(path) {
@@ -132,17 +159,19 @@ func main() {
 			case ".jar", ".war", ".ear":
 				f, err := os.Open(path)
 				if err != nil {
-					fmt.Fprintf(errFile, "can't open %s: %v", path, err)
+					_, _ = fmt.Fprintf(errFile, "can't open %s: %v", path, err)
 					return nil
 				}
-				defer f.Close()
-				sz, err := f.Seek(0, os.SEEK_END)
+				defer func(f *os.File) {
+					_ = f.Close()
+				}(f)
+				sz, err := f.Seek(0, io.SeekEnd)
 				if err != nil {
-					fmt.Fprintf(errFile, "can't seek in %s: %v", path, err)
+					_, _ = fmt.Fprintf(errFile, "can't seek in %s: %v", path, err)
 					return nil
 				}
-				if _, err := f.Seek(0, os.SEEK_END); err != nil {
-					fmt.Fprintf(errFile, "can't seek in %s: %v", path, err)
+				if _, err := f.Seek(0, io.SeekEnd); err != nil {
+					_, _ = fmt.Fprintf(errFile, "can't seek in %s: %v", path, err)
 					return nil
 				}
 				handleJar(path, f, sz)
@@ -154,6 +183,6 @@ func main() {
 	}
 
 	if !quiet {
-		fmt.Println("\nScan finished")
+		_, _ = fmt.Fprintf(os.Stderr, "\nScan finished\n")
 	}
 }
